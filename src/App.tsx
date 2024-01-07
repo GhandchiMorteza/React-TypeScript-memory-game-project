eb17e4b81bedbc60c2e2afac96d8449dac0df014
GhandchiMorteza <mortezaghandchi7@gmail.com>
2024-01-08 01:41:27 +0330

    Adding shuffle cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
+import { useState } from 'react';
 import './App.scss';
 
+interface Card {
+  id: number;
+  src: string;
+}
+
+const cardImages = [
+  { src: '/img/helmet-1.png' },
+  { src: '/img/potion-1.png' },
+  { src: '/img/ring-1.png' },
+  { src: '/img/scroll-1.png' },
+  { src: '/img/shield-1.png' },
+  { src: '/img/sword-1.png' },
+];
+
 function App() {
+  const [cards, setCards] = useState<Card[]>([]);
+  const [turns, setTurns] = useState(0);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card, index) => ({ ...card, id: index }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  console.log(cards, turns);
+
   return (
     <>
       <div className="App">
         <h1>بازی جادویی</h1>
-        <button>شروع مجدد</button>
+        <button onClick={shuffleCards}>شروع مجدد</button>
       </div>
     </>
   );
